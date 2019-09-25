21e2638cbf5cd50c1e9d312ce32f8851535b777b
syberpunkq <syberpunkq@gmail.com>
2019-09-26 01:34:17 +0300

    tolower & toupper fix

--- ft_tolower.c
+++ ft_tolower.c
@@ -3,19 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mzapdos <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: mzapdos <mzapdos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/21 20:41:18 by mzapdos           #+#    #+#             */
-/*   Updated: 2019/09/21 20:41:25 by mzapdos          ###   ########.fr       */
+/*   Updated: 2019/09/26 01:32:25 by mzapdos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_tolower(int c)
 {
-	char l;
-
-	l = (char)c;
-	if (l >= 'A' && l <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		c = c + 32;
 	return (c);
 }
--- ft_toupper.c
+++ ft_toupper.c
@@ -3,19 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_toupper.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mzapdos <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: mzapdos <mzapdos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/21 20:28:20 by mzapdos           #+#    #+#             */
-/*   Updated: 2019/09/21 20:38:45 by mzapdos          ###   ########.fr       */
+/*   Updated: 2019/09/26 01:31:37 by mzapdos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_toupper(int c)
 {
-	char l;
-
-	l = (char)c;
-	if (l >= 'a' && l <= 'z')
+	if (c >= 'a' && c <= 'z')
 		c = c - 32;
 	return (c);
 }

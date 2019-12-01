9bc130fabe3348eadcd15532ee714bcd7ce7efc2
syberpunkq <syberpunkq@gmail.com>
2019-12-01 10:58:11 +0300

    some changes

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstiter.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mzapdos <mzapdos@student.42.fr>            +#+  +:+       +#+        */
+/*   By: syberpunkq <syberpunkq@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/06 20:34:19 by mzapdos           #+#    #+#             */
-/*   Updated: 2019/10/06 20:34:44 by mzapdos          ###   ########.fr       */
+/*   Updated: 2019/12/01 10:56:55 by syberpunkq       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,9 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
 {
-	t_list	*next_lst;
-
 	while (lst)
 	{
-		next_lst = lst->next;
 		f(lst);
-		lst = next_lst;
+		lst = lst->next;
 	}
 }
